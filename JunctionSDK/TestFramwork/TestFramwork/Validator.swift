795b2a44c78fa56edca5d52862c0fadf42c23cb6
Davit <davidb@snowfalltravel.com>
2024-04-22 20:11:40 +0400

    added validator files

---
+++ JunctionSDK/TestFramwork/TestFramwork/Validator.swift
@@ -0,0 +1,17 @@
+//
+//  Validator.swift
+//  TestFramwork
+//
+//  Created by Davit on 22.04.24.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+}
