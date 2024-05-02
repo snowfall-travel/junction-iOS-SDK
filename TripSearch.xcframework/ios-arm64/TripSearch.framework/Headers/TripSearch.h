f8767f8971e650a16afd2d24f3c8ce816d609f54
Davit <davidb@snowfalltravel.com>
2024-05-02 12:24:10 +0400

    1.0.17

---
+++ TripSearch.xcframework/ios-arm64/TripSearch.framework/Headers/TripSearch.h
@@ -0,0 +1,18 @@
+//
+//  TripSearch.h
+//  TripSearch
+//
+//  Created by Davit on 06.02.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TripSearch.
+FOUNDATION_EXPORT double TripSearchVersionNumber;
+
+//! Project version string for TripSearch.
+FOUNDATION_EXPORT const unsigned char TripSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TripSearch/PublicHeader.h>
+
+
