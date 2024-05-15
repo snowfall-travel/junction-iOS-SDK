34533d131b8855708c7ef8cfb2cb0ef323ee4c54
Davit <davidb@snowfalltravel.com>
2024-05-15 10:14:17 +0400

    Added Alamofire

--- Package.swift
+++ Package.swift
@@ -11,6 +11,8 @@ let package = Package(
             name: "TestSDK",
             targets: ["TestSDK"]),
     ],
+    
+    dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.1")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
