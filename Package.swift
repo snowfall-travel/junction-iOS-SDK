6592710bd14fe6d80519260a0872cbb052a93301
Davit <davidb@snowfalltravel.com>
2024-05-15 12:22:59 +0400

    1.0.58

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-                    name: "TestSDK",
+                    name: "Alamofire",
                     dependencies: [
                         .product(name: "Alamofire", package: "Alamofire.swift")
                     ]
